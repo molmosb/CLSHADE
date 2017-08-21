093785c9dd3e24cc96c9d2e7ceb01e94396372c1
mzapata31 <mzapata.mca15@lania.edu.mx>
2017-08-21 16:20:45 -0500

    Función CLSHADE y estructura genérica de función Objetivo

---
+++ funcionObjetivo.m
@@ -0,0 +1,8 @@
+function [FO,SVR] = funcionObjetivo(Xi)
+    %Calculos preliminares
+    %---
+    %---
+    FO=resultado_FO; %colocar el resultado de la FO
+    SVR=resultado_SVR; %colocar resultado de la función
+end
+
